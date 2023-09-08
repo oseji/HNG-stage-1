c99eb108b35fc073872472ceb0da4723d7c2b037
oseji <jiade1233@gmail.com>
2023-09-08 16:14:46 +0100

    completed task

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 "use strict";
-console.log("hey");
 
-const time = new Date();
-console.log(time);
+const displayDay = document.querySelector(".day");
+const displayTime = document.querySelector(".time");
+
+const date = new Date();
+const daysOfTheWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const dayNum = date.getDay();
+const dayName = daysOfTheWeek[dayNum];
+
+displayDay.textContent = dayName;
+displayTime.textContent = date.getTime();
